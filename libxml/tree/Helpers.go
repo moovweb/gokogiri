1a44abd7938501d661a787449559ea9eaf28a361
Hampton Catlin <hcatlin@gmail.com>
2011-10-07 15:57:23 +0100

    Build a decent memory-safe string duper

--- libxml/tree/Helpers.go
+++ libxml/tree/Helpers.go
@@ -10,3 +10,10 @@ import "unsafe"
 func XmlChar2String(chars *C.xmlChar) string {
 	return C.GoString((*C.char)(unsafe.Pointer(chars)))
 }
+
+func String2XmlChar(input string) *C.xmlChar {
+	cString := C.CString(input)
+
+	defer C.free(unsafe.Pointer(cString))
+	return C.xmlCharStrdup(cString)
+}
