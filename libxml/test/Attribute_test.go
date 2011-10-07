f72323517c47aee93effe74e5d0c65d4cc85da2d
Hampton Catlin <hcatlin@gmail.com>
2011-10-07 12:38:09 +0100

    Adding in comments to make the test more logical

--- libxml/test/Attribute_test.go
+++ libxml/test/Attribute_test.go
@@ -26,21 +26,24 @@ func TestAttributeFetch(t *testing.T) {
 	if !(strings.Contains(doc.String(), "created=\"\"")) {
 		t.Error("Should have the 'created' attr in it")
 	}
-	exitingAttr, _ = node.Attribute("existing")
+
 	if exitingAttr.Name() != "existing" {
 		t.Error("Name isn't working with attributes")
 	}
-	exitingAttr.SetName("worked")
+	
+	exitingAttr.SetName("worked") // <node worked="true" created=""/>
+	
 	if !(strings.Contains(doc.String(), "worked=\"true\"")) {
 		t.Error("Should have the 'worked' attr in it")
 	}
 	if strings.Contains(doc.String(), "existing") {
 		t.Error("Existing attribute should be gone now")
 	}
+
 	// Remove the created attribute
-	createdAttr.Remove()
+	createdAttr.Remove() 	//<node worked="true"/>
 	if strings.Contains(doc.String(), "created") {
 		t.Error("Created attribute should be deleted now")
 	}
-	print(doc.String())
+	
 }
