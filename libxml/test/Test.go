7d53b41370f0dc8a2e6b50f5623aaca60ae95d40
Hampton Catlin <hcatlin@gmail.com>
2011-10-07 13:59:46 +0100

    move methods into the shared lib

--- libxml/test/Test.go
+++ libxml/test/Test.go
@@ -1,1 +1,20 @@
 package test
+
+import "testing"
+
+func AssertNil(t *testing.T, value interface{}, what string) {
+	if value != nil {
+		t.Error(what, "should be nil")
+	}
+}
+func Equal(t *testing.T, value, expected interface{}) {
+	if value != expected {
+		t.Error("Expected: ", expected, "\nBut got: ", value)
+	}
+}
+func Assert(t *testing.T, value interface{}, what string) interface{} {
+	if value == nil {
+		t.Error("Assertion failed: ", what)
+	}
+	return value
+}
--- libxml/test/Tree_test.go
+++ libxml/test/Tree_test.go
@@ -34,20 +34,3 @@ func TestTree(t *testing.T) {
 	// TODO: implement it so that they are ACTUALLY equal to each other.
 	Equal(t, parent.Next().String(), catLady.Prev().String())
 }
-
-func AssertNil(t *testing.T, value interface{}, what string) {
-	if value != nil {
-		t.Error(what, "should be nil")
-	}
-}
-func Equal(t *testing.T, value, expected interface{}) {
-	if value != expected {
-		t.Error("Expected: ", expected, "\nBut got: ", value)
-	}
-}
-func Assert(t *testing.T, value interface{}, what string) interface{} {
-	if value == nil {
-		t.Error("Assertion failed: ", what)
-	}
-	return value
-}
