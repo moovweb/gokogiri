231f4b10fc90967ef588dd5e8a63a9637fe4c2f6
Hampton Catlin <hcatlin@gmail.com>
2011-11-09 13:01:03 -0800

    added another text test

--- libxml/test/Text_test.go
+++ libxml/test/Text_test.go
@@ -15,4 +15,8 @@ func TestTextNodeContent(t *testing.T) {
 	if textNode.Content() != "hi" {
 		t.Error("Should be equal to the string 'hi'")
 	}
+	textNode.SetContent("mom")
+	if doc.First().String() != "<html>mom</html>" {
+		t.Error("Should be able to set text content")
+	}
 }
