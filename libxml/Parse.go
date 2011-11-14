f70087eaa36c54669a236a4411d71b0c26961a87
Zhigang Chen <zhigang.chen@moovweb.com>
2011-11-14 11:13:10 -0800

    simply move the bulk of parsing in Parse.go into tree/Doc.go

--- libxml/Parse.go
+++ libxml/Parse.go
@@ -1,35 +1,5 @@
 package libxml
-/*
-#include <libxml/HTMLparser.h> 
-#include <libxml/HTMLtree.h>
-
-xmlDoc* 
-htmlDocToXmlDoc(htmlDocPtr doc) { return (xmlDocPtr)doc; }
-*/
-import "C"
-import "unsafe"
 import . "libxml/tree"
-
-func HtmlParseStringWithOptions(content string, url string, encoding string, opts int) *Doc {
-	contentCharPtr := C.CString(content)
-	defer C.free(unsafe.Pointer(contentCharPtr))
-	contentXmlCharPtr := C.xmlCharStrdup(contentCharPtr)
-	defer XmlFreeChars(unsafe.Pointer(contentXmlCharPtr))
-	urlCharPtr := C.CString(url)
-	defer C.free(unsafe.Pointer(urlCharPtr))
-
-	var encodingCharPtr *C.char = nil
-	if encoding != "" {
-		encodingCharPtr = C.CString(encoding)
-		defer C.free(unsafe.Pointer(encodingCharPtr))
-	}
-	htmlDocPtr := C.htmlReadDoc(contentXmlCharPtr, urlCharPtr, encodingCharPtr, C.int(opts))
-	if htmlDocPtr == nil {
-		return nil
-	}
-	xmlDocPtr := C.htmlDocToXmlDoc(htmlDocPtr)
-	return NewDoc(unsafe.Pointer(xmlDocPtr))
-}
 
 func HtmlParseString(content string) *Doc {
 	doc := HtmlParseStringWithOptions(content, "", "",
@@ -41,32 +11,6 @@ func HtmlParseString(content string) *Doc {
 		return HtmlParseString("<html />")
 	}
 	return doc
-}
-
-func HtmlParseFile(url string, encoding string, opts int) *Doc {
-	htmlDocPtr := C.htmlReadFile(C.CString(url), C.CString(encoding), C.int(opts))
-	xmlDocPtr := C.htmlDocToXmlDoc(htmlDocPtr)
-	if xmlDocPtr == nil {
-		return nil
-	}
-	return NewDoc(unsafe.Pointer(xmlDocPtr))
-}
-
-func XmlParseWithOption(content string, url string, encoding string, opts int) *Doc {
-	contentCharPtr := C.CString(content)
-	defer C.free(unsafe.Pointer(contentCharPtr))
-	contentXmlCharPtr := C.xmlCharStrdup(contentCharPtr)
-	defer XmlFreeChars(unsafe.Pointer(contentXmlCharPtr))
-	urlCharPtr := C.CString(url)
-	defer C.free(unsafe.Pointer(urlCharPtr))
-
-	var encodingCharPtr *C.char = nil
-	if encoding != "" {
-		encodingCharPtr = C.CString(encoding)
-		defer C.free(unsafe.Pointer(encodingCharPtr))
-	}
-	xmlDocPtr := C.xmlReadDoc(contentXmlCharPtr, urlCharPtr, encodingCharPtr, C.int(opts))
-	return NewDoc(unsafe.Pointer(xmlDocPtr))
 }
 
 func XmlParseString(content string) *Doc {
--- libxml/tree/Doc.go
+++ libxml/tree/Doc.go
@@ -26,6 +26,7 @@ DumpHtmlToString(xmlDoc *doc) {
 }
 
 xmlNode * GoXmlCastDocToNode(xmlDoc *doc) { return (xmlNode *)doc; }
+xmlDoc * htmlDocToXmlDoc(htmlDocPtr doc) { return (xmlDocPtr)doc; }
 */
 import "C"
 import "unsafe"
@@ -40,6 +41,55 @@ func Parse(input string) *Doc {
 	defer C.free(unsafe.Pointer(cCharInput))
 	doc := C.xmlParseMemory(cCharInput, C.int(len(input)))
 	return NewNode(unsafe.Pointer(doc), nil).(*Doc)
+}
+
+func XmlParseWithOption(content string, url string, encoding string, opts int) *Doc {
+	contentCharPtr := C.CString(content)
+	defer C.free(unsafe.Pointer(contentCharPtr))
+	contentXmlCharPtr := C.xmlCharStrdup(contentCharPtr)
+	defer XmlFreeChars(unsafe.Pointer(contentXmlCharPtr))
+	urlCharPtr := C.CString(url)
+	defer C.free(unsafe.Pointer(urlCharPtr))
+
+	var encodingCharPtr *C.char = nil
+	if encoding != "" {
+		encodingCharPtr = C.CString(encoding)
+		defer C.free(unsafe.Pointer(encodingCharPtr))
+	}
+	xmlDocPtr := C.xmlReadDoc(contentXmlCharPtr, urlCharPtr, encodingCharPtr, C.int(opts))
+	return NewDoc(unsafe.Pointer(xmlDocPtr))
+}
+
+
+func HtmlParseStringWithOptions(content string, url string, encoding string, opts int) *Doc {
+	contentCharPtr := C.CString(content)
+	defer C.free(unsafe.Pointer(contentCharPtr))
+	contentXmlCharPtr := C.xmlCharStrdup(contentCharPtr)
+	defer XmlFreeChars(unsafe.Pointer(contentXmlCharPtr))
+	urlCharPtr := C.CString(url)
+	defer C.free(unsafe.Pointer(urlCharPtr))
+
+	var encodingCharPtr *C.char = nil
+	if encoding != "" {
+		encodingCharPtr = C.CString(encoding)
+		defer C.free(unsafe.Pointer(encodingCharPtr))
+	}
+	htmlDocPtr := C.htmlReadDoc(contentXmlCharPtr, urlCharPtr, encodingCharPtr, C.int(opts))
+	if htmlDocPtr == nil {
+		return nil
+	}
+	xmlDocPtr := C.htmlDocToXmlDoc(htmlDocPtr)
+	return NewDoc(unsafe.Pointer(xmlDocPtr))
+}
+
+
+func HtmlParseFile(url string, encoding string, opts int) *Doc {
+	htmlDocPtr := C.htmlReadFile(C.CString(url), C.CString(encoding), C.int(opts))
+	xmlDocPtr := C.htmlDocToXmlDoc(htmlDocPtr)
+	if xmlDocPtr == nil {
+		return nil
+	}
+	return NewDoc(unsafe.Pointer(xmlDocPtr))
 }
 
 func CreateHtmlDoc() *Doc {
