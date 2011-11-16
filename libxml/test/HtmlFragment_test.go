77b10b1807b5f26552f58d4574237dacf7fed304
Zhigang Chen <zhigang.chen@moovweb.com>
2011-11-15 16:56:47 -0800

    reorg and add more tests for html fragment parsing

--- libxml/test/HtmlFragment_test.go
+++ libxml/test/HtmlFragment_test.go
@@ -3,14 +3,50 @@ package test
 import (
 	"libxml"
 	"testing"
-	"libxml/tree"
 	"libxml/help"
-	"strings"
 )
 
 func TestHtmlFragment(t *testing.T) {
-	children := ParseHtmlFragment("<body><div></body>")
-	Equal(t, children[0].Name(), "body")
-	Equal(t, children[0].First().Name(), "div")
-	Equal(t, children[0].String(), "<body><div></body>")
+	doc := libxml.XmlParseString("<meta name=\"format-detection\" content=\"telephone=no\">")
+	root := doc.RootElement()
+	child := doc.NewElement("child")
+	root.AppendChildNode(child)
+	Equal(t, root.String(), "<meta name=\"format-detection\" content=\"telephone=no\"><child/></meta>")
+	doc.Free()
+
+	help.XmlCleanUpParser()
+	if help.XmlMemoryAllocation() != 0 {
+		t.Errorf("Memeory leaks %d!!!", help.XmlMemoryAllocation())
+		help.XmlMemoryLeakReport()
+	}
+}
+
+func TestHtmlFragment2(t *testing.T) {
+	doc := libxml.HtmlParseFragment("<body><div/></body>")
+  f := doc.RootElement().First()
+	Equal(t, f.Name(), "body")
+	Equal(t, f.First().Name(), "div")
+	Equal(t, f.String(), "<body><div/></body>")
+  doc.Free()
+
+  help.XmlCleanUpParser()
+  if help.XmlMemoryAllocation() != 0 {
+    t.Errorf("Memeory leaks %d!!!", help.XmlMemoryAllocation())
+    help.XmlMemoryLeakReport()
+  }
+}
+
+func TestHtmlFragment3(t *testing.T) {
+	doc := libxml.HtmlParseFragment("<h1><div/></h1>")
+  f := doc.RootElement().First()
+	Equal(t, f.Name(), "h1")
+	Equal(t, f.First().Name(), "div")
+	Equal(t, f.String(), "<h1><div/></h1>")
+  doc.Free()
+
+  help.XmlCleanUpParser()
+  if help.XmlMemoryAllocation() != 0 {
+    t.Errorf("Memeory leaks %d!!!", help.XmlMemoryAllocation())
+    help.XmlMemoryLeakReport()
+  }
 }
