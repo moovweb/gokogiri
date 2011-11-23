2965d4688ab1f4ee9b86b4a465cdefdf2abc0114
Zhigang Chen <zhigang.chen@moovweb.com>
2011-11-23 12:09:52 -0800

    need to mark a node's attribute nodes when deleting it

--- libxml/tree/Callback.c
+++ libxml/tree/Callback.c
@@ -4,7 +4,17 @@
 
 void invalidTree(xmlNode *node, void *doc) {
     xmlNode *cur_node = NULL;
+    xmlAttr *cur_attr = NULL;
     if (node != NULL) {
+        //clear all attributes
+        cur_attr = node->properties;
+        while (cur_attr != NULL) {
+            //just mark it as deleted in the map
+            //all properties will be deleted along with this node
+            invalidNode(cur_attr, doc);
+            cur_attr = cur_attr->next;
+        }
+        //recursively clear all children
         cur_node = node->children;
         while(cur_node != NULL) {
             invalidTree(cur_node, doc);
