5b916d940522970b7ab76ac6f2efa7c67e5acd98
Zhigang Chen <zhigang.chen@moovweb.com>
2012-02-03 14:41:30 -0800

    no recursive call but use iterations.

--- libxml/tree/Callback.c
+++ libxml/tree/Callback.c
@@ -1,29 +1,40 @@
 #include <libxml/tree.h>
 #include "Callback.h"
 #include <stdio.h>
+#include <assert.h>
 
-void invalidTree(xmlNode *node, void *doc) {
-    xmlNode *cur_node = NULL;
-    xmlAttr *cur_attr = NULL;
+void clearXmlNode(xmlNode *node, void *doc) {
+	xmlAttr *attr = NULL;
+	//clear all attributes
+	attr = node->properties;
+	while (attr != NULL) {
+	    //just mark it as deleted in the map
+	    //all properties will be deleted along with this node
+	    invalidateNode(attr, doc);
+	    attr = attr->next;
+	}
+	invalidateNode(node, doc);
+	if (node->parent != NULL) {
+	    xmlUnlinkNode(node);
+	}
+	xmlFreeNode(node);
+}
+
+void invalidateTree(xmlNode *node, void *doc) {
     if (node != NULL) {
-        //clear all attributes
-        cur_attr = node->properties;
-        while (cur_attr != NULL) {
-            //just mark it as deleted in the map
-            //all properties will be deleted along with this node
-            invalidNode(cur_attr, doc);
-            cur_attr = cur_attr->next;
-        }
-        //recursively clear all children
-        cur_node = node->children;
-        while(cur_node != NULL) {
-            invalidTree(cur_node, doc);
-            cur_node = node->children;            
-        }
-        invalidNode(node, doc);
-        if (node->parent != NULL) {
-            xmlUnlinkNode(node);
-        }
-        xmlFreeNode(node);
+	    xmlNode *cur_node = node, *node_to_clear = node;
+		do {
+			while(cur_node->children != NULL) {
+				cur_node = cur_node->children;
+			}
+			while(cur_node->children == NULL && cur_node != node) {
+				node_to_clear = cur_node;
+				cur_node = cur_node->parent;
+				clearXmlNode(node_to_clear, doc);
+			}
+		} while (cur_node->children != NULL);
+
+		assert(node == cur_node);
+		clearXmlNode(cur_node, doc);
     }
 }
