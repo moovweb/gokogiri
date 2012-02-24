f937686dfabe3ea358bbc66fa2277a703dae9379
Zhigang Chen <zhigang.chen@moovweb.com>
2012-02-24 11:51:21 -0800

    HC, update the tests and make them pass

--- libxml/test/Doc_test.go
+++ libxml/test/Doc_test.go
@@ -35,7 +35,7 @@ func TestXmlDocWithComment(t *testing.T) {
 func TestNewElement(t *testing.T) {
 	doc := libxml.XmlParseString("<root>hi</root>")
 	root := doc.RootElement()
-	child := doc.NewElement("child")
+	child, _ := doc.NewElement("child")
 	root.AppendChildNode(child)
 	Equal(t, root.String(), "<root>hi<child/></root>")
 	doc.Free()
