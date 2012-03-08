90b05e42ea668faf63a3703c4b87b196411f427c
Zhigang Chen <zhigang.chen@moovweb.com>
2012-03-08 10:51:11 -0800

    add test for fragment

---
+++ xml/fragment_test.go
@@ -0,0 +1,55 @@
+package xml
+
+import (
+	"testing"
+	"gokogiri/help"
+)
+
+func TestParseDocumentFragment(t *testing.T) {
+	doc, err := Parse(nil, nil, []byte("utf-8"), DefaultParseOption)
+	if err != nil {
+		println(err.String())
+	}
+	docFragment, err := doc.ParseFragment([]byte("<foo></foo><!-- comment here --><bar>fun</bar>"), nil, DefaultParseOption)
+	if err != nil {
+		t.Error(err.String())
+	}
+	if (docFragment.Children.Length() != 3) {
+		t.Error("the number of children from the fragment does not match")
+	}
+
+	doc.Free()
+	help.CheckXmlMemoryLeaks(t)
+	
+}
+
+func TestSearchDocumentFragment(t *testing.T) {
+	doc, err := Parse(nil, nil, []byte("utf-8"), DefaultParseOption)
+	if err != nil {
+		println(err.String())
+	}
+	docFragment, err := doc.ParseFragment([]byte("<foo></foo><!-- comment here --><bar>fun</bar>"), nil, DefaultParseOption)
+	if err != nil {
+		t.Error(err.String())
+	}
+	nodes, err := docFragment.Search(".//*")
+	if err != nil {
+		t.Error("fragment search has error")
+	}
+	if len(nodes) != 2 {
+		t.Error("the number of children from the fragment does not match")
+	}
+	nodes, err = docFragment.Search("//*")
+
+	if err != nil {
+		t.Error("fragment search has error")
+	}
+
+	if len(nodes) != 0 {
+		t.Error("the number of children from the fragment's document does not match")
+	}
+
+	doc.Free()
+	help.CheckXmlMemoryLeaks(t)
+	
+}
