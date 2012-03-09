799e8d0f35c04ff0ca7dfae21454d16375882aeb
Sean Jezewski <sean@moovweb.com>
2012-03-09 15:47:48 -0800

    Add back buffered test

--- xml/document_test.go
+++ xml/document_test.go
@@ -14,7 +14,7 @@ func TestDocuments(t *testing.T) {
 	
 	errors := make([]string, 0)
 
-	print("\nTesting: [")
+	print("\nTesting: Basic Parsing [")
 
 	for _, test := range(tests) {
 		error := RunDocumentParseTest(t, test)
@@ -37,6 +37,68 @@ func TestDocuments(t *testing.T) {
 	}
 }
 
+func TestBufferedDocuments(t *testing.T) {
+	tests := collectTests(t)
+	
+	errors := make([]string, 0)
+
+	print("\nTesting: Buffered Parsing [")
+
+	for _, test := range(tests) {
+		error := RunParseDocumentWithBufferTest(t, test)
+
+		if error != nil {
+			errors = append(errors, fmt.Sprintf("Test %v failed:\n%v\n", test, *error))
+			print("F")
+		} else {
+			print(".")
+		}
+	}
+	
+	println("]")
+
+	if len(errors) > 0 {
+		errorMessage := "\t" + strings.Join( strings.Split(strings.Join(errors, "\n\n"), "\n"), "\n\t")
+		t.Errorf("\nSome tests failed! (%d passed / %d total) :\n%v", len(tests) - len(errors), len(tests), errorMessage)
+	} else {
+		fmt.Printf("\nAll (%d) tests passed!\n", len(tests))
+	}
+}
+
+func RunParseDocumentWithBufferTest(t *testing.T, name string) (error *string) {
+	var errorMessage string
+	offset := "\t"
+
+	defer help.CheckXmlMemoryLeaks(t)
+
+	input, output, dataError := getTestData(name)
+
+	if len(dataError) > 0 {
+		errorMessage += dataError
+	}
+	
+	buffer := make([]byte, 100)
+
+	doc, err := ParseWithBuffer(input, DefaultEncodingBytes, nil, DefaultParseOption, DefaultEncodingBytes, buffer)
+
+	if err != nil {
+		errorMessage = fmt.Sprintf("parsing error:%v\n", err)
+	}
+	
+	if doc.String() != string(output) {
+		formattedOutput := offset + strings.Join(strings.Split("[" + doc.String() + "]", "\n"), "\n" + offset)
+		formattedExpectedOutput := offset + strings.Join(strings.Split("[" + string(output) + "]", "\n"), "\n" + offset)
+		errorMessage = fmt.Sprintf("%v-- Got --\n%v\n%v-- Expected --\n%v\n", offset, formattedOutput, offset, formattedExpectedOutput)
+	}
+	doc.Free()	
+
+	if len(errorMessage) > 0 {
+		return &errorMessage	
+	} 
+	return nil
+
+}
+
 func RunDocumentParseTest(t *testing.T, name string) (error *string) {
 
 	var errorMessage string
@@ -44,18 +106,11 @@ func RunDocumentParseTest(t *testing.T, name string) (error *string) {
 
 	defer help.CheckXmlMemoryLeaks(t)
 
-	input, err := ioutil.ReadFile(filepath.Join(name, "input.txt"))
-	
-	if err != nil {
-		errorMessage += fmt.Sprintf("%vCouldn't read test (%v) input:\n%v\n", offset, name, offset + err.String())
-	}
-	
-	output, err := ioutil.ReadFile(filepath.Join(name, "output.txt"))
-	
-	if err != nil {
-		errorMessage += fmt.Sprintf("%vCouldn't read test (%v) output:\n%v\n", offset, name, offset + err.String())
-	}
+	input, output, dataError := getTestData(name)
 
+	if len(dataError) > 0 {
+		errorMessage += dataError
+	}
 
 	doc, err := Parse(input, DefaultEncodingBytes, nil, DefaultParseOption, DefaultEncodingBytes)
 
@@ -98,7 +153,21 @@ func collectTests(t *testing.T) (names []string) {
 	return
 }
 
+func getTestData(name string) (input []byte, output []byte, error string) {
+	var errorMessage string
+	offset := "\t"
 
+	input, err := ioutil.ReadFile(filepath.Join(name, "input.txt"))
+	
+	if err != nil {
+		errorMessage += fmt.Sprintf("%vCouldn't read test (%v) input:\n%v\n", offset, name, offset + err.String())
+	}
+	
+	output, err = ioutil.ReadFile(filepath.Join(name, "output.txt"))
+	
+	if err != nil {
+		errorMessage += fmt.Sprintf("%vCouldn't read test (%v) output:\n%v\n", offset, name, offset + err.String())
+	}
 
-func TestBufferedDocuments(t *testing.T) {
+	return input, output, errorMessage
 }
