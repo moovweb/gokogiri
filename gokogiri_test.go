1c595f3f78a5a97d0a33b186523f27eed860955a
Zhigang Chen <zhigang.chen@moovweb.com>
2012-05-22 16:12:32 -0700

    more the tests a little more interesting

--- gokogiri_test.go
+++ gokogiri_test.go
@@ -15,9 +15,21 @@ func TestParseHtml(t *testing.T) {
 		t.Error("Parsing has error:", err)
 		return
 	}
+	if doc.String() != expected {
+		t.Error("the output of the html doc does not match the expected")
+	}
+
+	expected = `<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"></head>
+<body><div><h1></h1></div></body>
+</html>
+`
+	doc.Root().FirstChild().AddPreviousSibling("<head></head>")
 
 	if doc.String() != expected {
-		t.Error("the output of the html doc does not match the empty xml")
+		println(doc.String())
+		t.Error("the output of the html doc does not match the expected")
 	}
 	doc.Free()
 	help.CheckXmlMemoryLeaks(t)
@@ -35,7 +47,17 @@ func TestParseXml(t *testing.T) {
 	}
 
 	if doc.String() != expected {
-		t.Error("the output of the html doc does not match the empty xml")
+		t.Error("the output of the xml doc does not match the expected")
+	}
+
+	expected = `<?xml version="1.0" encoding="utf-8"?>
+<foo>
+  <bar/>
+</foo>
+`
+	doc.Root().AddChild("<bar/>")
+	if doc.String() != expected {
+		t.Error("the output of the xml doc does not match the expected")
 	}
 	doc.Free()
 	help.CheckXmlMemoryLeaks(t)
