1dd671951429f2265ea74f2b80e9ddda531d1392
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-07-12 00:41:10 -0700

    Add syntax check function

--- xpath/expression.go
+++ xpath/expression.go
@@ -3,14 +3,41 @@ package xpath
 /*
 #include <libxml/xpath.h> 
 #include <libxml/xpathInternals.h>
+
+void check_xpath_syntax_noop() {
+}
+
+char *check_xpath_syntax(const char *xpath) {
+	char *rval = NULL;
+	xmlXPathContextPtr ctx = xmlXPathNewContext(NULL);
+	ctx->error = check_xpath_syntax_noop;
+	xmlXPathCtxtCompile(ctx, (const xmlChar *)xpath);
+	if (ctx->lastError.domain > 0) {
+		//fprintf(stderr, "%s Code: %d Domain: %d", ctx->lastError.str2, ctx->lastError.code, ctx->lastError.domain);
+		rval = "ERROR";
+	}
+	xmlXPathFreeContext(ctx);
+	return rval;
+}
 */
 import "C"
 import "unsafe"
 import . "gokogiri/util"
 import "runtime"
+import "errors"
 
 type Expression struct {
 	Ptr *C.xmlXPathCompExpr
+}
+
+func Check(path string) (err error) {
+	str := C.CString(path)
+	defer C.free(unsafe.Pointer(str))
+	cstr := C.check_xpath_syntax(str)
+	if cstr != nil {
+		err = errors.New(C.GoString(cstr))
+	}
+	return
 }
 
 func Compile(path string) (expr *Expression) {
