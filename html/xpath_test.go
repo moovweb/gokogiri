6931b1f1260b84025710961920fe1ff1b4b8432f
Zhigang Chen <zhigang.chen@moovweb.com>
2012-10-16 09:43:04 -0700

    update test cases.

--- html/xpath_test.go
+++ html/xpath_test.go
@@ -15,8 +15,29 @@ func TestUnfoundFuncInXpath(t *testing.T) {
 
 	html := doc.Root().FirstChild()
 	results, _ := html.Search("./div[matches(text(), 'foo')]")
-	if len(results) != 0 {
-		t.Error("should match nothing because the function is not found")
+	if results != nil {
+		t.Error("should return nil because the function is not found")
+	}
+	doc.Free()
+}
+
+func TestXpathEmptyResult(t *testing.T) {
+	defer CheckXmlMemoryLeaks(t)
+
+	doc, err := Parse([]byte("<html><body><div><h1></div>"), DefaultEncodingBytes, nil, DefaultParseOption, DefaultEncodingBytes)
+
+	if err != nil {
+		t.Error("Parsing has error:", err)
+		return
+	}
+
+	html := doc.Root().FirstChild()
+	results, err := html.Search("./div[@calass='cool']")
+	if err != nil {
+		t.Error("Xpath eval should not return nil")
+	}
+	if len(results) > 0 {
+		t.Error("Xpath should return empty result")
 	}
 	doc.Free()
 }
