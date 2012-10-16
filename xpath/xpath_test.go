740110eba33c2b03e494589d6851f441bc77f065
Zhigang Chen <zhigang.chen@moovweb.com>
2012-10-15 17:14:06 -0700

    add two tests for compiling XPath expression

--- xpath/xpath_test.go
+++ xpath/xpath_test.go
@@ -1,3 +1,20 @@
 package xpath
 
 //please check the search tests in gokogiri/xml and gokogiri/html
+import "testing"
+
+func TestCompileGoodExpr(t *testing.T) {
+	defer CheckXmlMemoryLeaks(t)
+	e := Compile(`./*`)
+	if e == nil {
+		t.Error("expr should be good")
+	}
+}
+
+func TestCompileBadExpr(t *testing.T) {
+	defer CheckXmlMemoryLeaks(t)
+	e := Compile("./")
+	if e != nil {
+		t.Error("expr should be bad")
+	}
+}
