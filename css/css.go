71adb54929653fa1a6dc361c83a52ed3b971c3a9
Zhigang Chen <zhigang.chen@moovweb.com>
2012-11-08 10:50:57 -0800

    the regex objects cannot be shared and must be created dynamically

--- css/css.go
+++ css/css.go
@@ -61,7 +61,21 @@ const (
 var pattern [NUM_LEXEMES]string
 var matcher [NUM_LEXEMES]*rubex.Regexp
 
-func init() {
+type Scope int
+
+const (
+  GLOBAL = iota
+  LOCAL
+)
+
+func Convert(css string, scope Scope) string {
+  matchers := allocate()
+  defer deallocate(matchers)
+  xpath, _ := selectors(matchers, []byte(css), scope)
+  return xpath
+}
+
+func allocate() []*rubex.Regexp {
   // some overlap in here, but it'll make the parsing functions clearer
   pattern[SPACES] = `\s+`
   pattern[COMMA] = `\s*,`
@@ -107,67 +121,64 @@ func init() {
   pattern[PRECEDES] = `\s*~`
   pattern[PARENT_OF] = `\s*>`
   pattern[ANCESTOR_OF] = `\s+`
-  for i, p := range pattern {
-    matcher[i] = rubex.MustCompile(`\A` + p)
+  matchers := make([]*rubex.Regexp, 0, NUM_LEXEMES)
+  for _, p := range pattern {
+    matchers = append(matchers, rubex.MustCompile(`\A` + p))
+  }
+  return matchers
+}
+
+func deallocate(matchers []*rubex.Regexp) {
+  for _, m := range matchers {
+    m.Free()
   }
 }
 
-type Scope int
 
-const (
-  GLOBAL = iota
-  LOCAL
-)
-
-func Convert(css string, scope Scope) string {
-  xpath, _ := selectors([]byte(css), scope)
-  return xpath
-}
-
-func selectors(input []byte, scope Scope) (string, []byte) {
-  x, input := selector(input, scope)
+func selectors(matchers []*rubex.Regexp, input []byte, scope Scope) (string, []byte) {
+  x, input := selector(matchers, input, scope)
   xs := []string{x}
-  for peek(COMMA, input) {
-    _, input = token(COMMA, input)
-    x, input = selector(input, scope)
+  for peek(matchers, COMMA, input) {
+    _, input = token(matchers, COMMA, input)
+    x, input = selector(matchers, input, scope)
     xs = append(xs, x)
   }
   return strings.Join(xs, " | "), input
 }
 
-func selector(input []byte, scope Scope) (string, []byte) {
+func selector(matchers []*rubex.Regexp, input []byte, scope Scope) (string, []byte) {
   var combinator Lexeme
   var xs []string
   if scope == LOCAL {
     xs = []string{"."}
   }
-  if matched, remainder := token(PARENT_OF, input); matched != nil {
+  if matched, remainder := token(matchers, PARENT_OF, input); matched != nil {
     combinator, input = PARENT_OF, remainder
   } else {
     combinator = ANCESTOR_OF
   }
-  x, input := sequence(input, combinator)
+  x, input := sequence(matchers, input, combinator)
   xs = append(xs, x)
   for {
-    if matched, remainder := token(ADJACENT_TO, input); matched != nil {
+    if matched, remainder := token(matchers, ADJACENT_TO, input); matched != nil {
       combinator, input = ADJACENT_TO, remainder
-    } else if matched, remainder := token(PRECEDES, input); matched != nil {
+    } else if matched, remainder := token(matchers, PRECEDES, input); matched != nil {
       combinator, input = PRECEDES, remainder
-    } else if matched, remainder := token(PARENT_OF, input); matched != nil {
+    } else if matched, remainder := token(matchers, PARENT_OF, input); matched != nil {
       combinator, input = PARENT_OF, remainder
-    } else if matched, remainder := token(ANCESTOR_OF, input); matched != nil {
+    } else if matched, remainder := token(matchers, ANCESTOR_OF, input); matched != nil {
       combinator, input = ANCESTOR_OF, remainder
     } else {
       break
     }
-    x, input = sequence(input, combinator)
+    x, input = sequence(matchers, input, combinator)
     xs = append(xs, x)
   }
   return strings.Join(xs, ""), input
 }
 
-func sequence(input []byte, combinator Lexeme) (string, []byte) {
-  _, input = token(SPACES, input)
+func sequence(matchers []*rubex.Regexp, input []byte, combinator Lexeme) (string, []byte) {
+  _, input = token(matchers, SPACES, input)
   x, ps := "", []string{}
 
   switch combinator {
@@ -182,13 +193,13 @@ func sequence(input []byte, combinator Lexeme) (string, []byte) {
     ps = append(ps, "position()=1")
   }
 
-  if e, remainder := token(ELEMENT, input); e != nil {
+  if e, remainder := token(matchers, ELEMENT, input); e != nil {
     input = remainder
     if len(ps) > 0 {
       ps = append(ps, " and ")
     }
     ps = append(ps, "self::"+string(e))
-    if !(peek(ID, input) || peek(CLASS, input) || peek(PSEUDO_CLASS, input) || peek(LBRACKET, input)) {
+    if !(peek(matchers, ID, input) || peek(matchers, CLASS, input) || peek(matchers, PSEUDO_CLASS, input) || peek(matchers, LBRACKET, input)) {
       pstr := strings.Join(ps, "")
       if pstr != "" {
         pstr = fmt.Sprintf("[%s]", pstr)
@@ -196,7 +207,7 @@ func sequence(input []byte, combinator Lexeme) (string, []byte) {
       return x + pstr, input
     }
   }
-  q, input, connective := qualifier(input)
+  q, input, connective := qualifier(matchers, input)
   if q == "" {
     panic("Invalid CSS selector")
   }
@@ -204,7 +215,7 @@ func sequence(input []byte, combinator Lexeme) (string, []byte) {
     ps = append(ps, connective)
   }
   ps = append(ps, q)
-  for q, r, c := qualifier(input); q != ""; q, r, c = qualifier(input) {
+  for q, r, c := qualifier(matchers, input); q != ""; q, r, c = qualifier(matchers, input) {
     ps, input = append(ps, c, q), r
   }
   pstr := strings.Join(ps, "")
@@ -214,25 +225,25 @@ func sequence(input []byte, combinator Lexeme) (string, []byte) {
   return x + pstr, input
 }
 
-func qualifier(input []byte) (string, []byte, string) {
+func qualifier(matchers []*rubex.Regexp, input []byte) (string, []byte, string) {
   p, connective := "", ""
-  if t, remainder := token(CLASS, input); t != nil {
+  if t, remainder := token(matchers, CLASS, input); t != nil {
     p = fmt.Sprintf(`contains(concat(" ", @class, " "), " %s ")`, string(t[1:]))
     input = remainder
     connective = " and "
-  } else if t, remainder := token(ID, input); t != nil {
+  } else if t, remainder := token(matchers, ID, input); t != nil {
     p, input, connective = fmt.Sprintf(`@id="%s"`, string(t[1:])), remainder, " and "
-  } else if peek(PSEUDO_CLASS, input) {
-    p, input, connective = pseudoClass(input)
-  } else if peek(LBRACKET, input) {
-    p, input = attribute(input)
+  } else if peek(matchers, PSEUDO_CLASS, input) {
+    p, input, connective = pseudoClass(matchers, input)
+  } else if peek(matchers, LBRACKET, input) {
+    p, input = attribute(matchers, input)
     connective = " and "
   }
   return p, input, connective
 }
 
-func pseudoClass(input []byte) (string, []byte, string) {
-  class, input := token(PSEUDO_CLASS, input)
+func pseudoClass(matchers []*rubex.Regexp, input []byte) (string, []byte, string) {
+  class, input := token(matchers, PSEUDO_CLASS, input)
   var p, connective string
   switch string(class) {
   case ":first-child":
@@ -248,13 +259,13 @@ func pseudoClass(input []byte) (string, []byte, string) {
   case ":only-of-type":
     p, connective = "position() = 1 and position() = last()", "]["
   case ":nth-child":
-    p, input = nth(input)
+    p, input = nth(matchers, input)
     connective = " and "
   case ":nth-of-type":
-    p, input = nth(input)
+    p, input = nth(matchers, input)
     connective = "]["
   case ":not":
-    p, input = negate(input)
+    p, input = negate(matchers, input)
     connective = " and "
   default:
     panic(`Cannot convert CSS pseudo-class "` + string(class) + `" to XPath.`)
@@ -262,40 +273,40 @@ func pseudoClass(input []byte) (string, []byte, string) {
   return p, input, connective
 }
 
-func nth(input []byte) (string, []byte) {
-  lparen, input := token(LPAREN, input)
+func nth(matchers []*rubex.Regexp, input []byte) (string, []byte) {
+  lparen, input := token(matchers, LPAREN, input)
   if lparen == nil {
     panic(":nth-child and :nth-of-type require an parenthesized argument")
   }
-  _, input = token(SPACES, input)
+  _, input = token(matchers, SPACES, input)
   var expr string
-  if e, rem := token(EVEN, input); e != nil {
+  if e, rem := token(matchers, EVEN, input); e != nil {
     expr, input = "position() mod 2 = 0", rem
-  } else if e, rem := token(ODD, input); e != nil {
+  } else if e, rem := token(matchers, ODD, input); e != nil {
     expr, input = "position() mod 2 = 1", rem
-  } else if e, _ := token(BINOMIAL, input); e != nil {
+  } else if e, _ := token(matchers, BINOMIAL, input); e != nil {
     var coefficient, operator, constant []byte
-    coefficient, input = token(COEFFICIENT, input)
+    coefficient, input = token(matchers, COEFFICIENT, input)
     switch string(coefficient) {
     case "", "+":
       coefficient = []byte("1")
     case "-":
       coefficient = []byte("-1")
     }
-    _, input = token(N, input)
-    _, input = token(SPACES, input)
-    operator, input = token(OPERATOR, input)
-    _, input = token(SPACES, input)
-    constant, input = token(UNSIGNED, input)
+    _, input = token(matchers, N, input)
+    _, input = token(matchers, SPACES, input)
+    operator, input = token(matchers, OPERATOR, input)
+    _, input = token(matchers, SPACES, input)
+    constant, input = token(matchers, UNSIGNED, input)
     expr = fmt.Sprintf("(position() %s %s) mod %s = 0", invert(string(operator)), string(constant), string(coefficient))
-  } else if e, rem := token(SIGNED, input); e != nil {
+  } else if e, rem := token(matchers, SIGNED, input); e != nil {
     expr, input = "position() = "+string(e), rem
   } else {
     panic("Invalid argument to :nth-child or :nth-of-type.")
   }
   fmt.Println(string(input))
-  _, input = token(SPACES, input)
-  rparen, input := token(RPAREN, input)
+  _, input = token(matchers, SPACES, input)
+  rparen, input := token(matchers, RPAREN, input)
   if rparen == nil {
     panic("Unterminated argument to :nth-child or :nth-of-type.")
   }
@@ -312,44 +323,44 @@ func invert(op string) string {
   return op
 }
 
-func negate(input []byte) (string, []byte) {
-  _, input = token(SPACES, input)
-  lparen, input := token(LPAREN, input)
+func negate(matchers []*rubex.Regexp, input []byte) (string, []byte) {
+  _, input = token(matchers, SPACES, input)
+  lparen, input := token(matchers, LPAREN, input)
   if lparen == nil {
     panic(":not requires a parenthesized argument.")
   }
-  _, input = token(SPACES, input)
-  p, input := sequence(input, NOT)
-  _, input = token(SPACES, input)
-  rparen, input := token(RPAREN, input)
+  _, input = token(matchers, SPACES, input)
+  p, input := sequence(matchers, input, NOT)
+  _, input = token(matchers, SPACES, input)
+  rparen, input := token(matchers, RPAREN, input)
   if rparen == nil {
     panic("Unterminated argument to :not.")
   }
   return fmt.Sprintf("not(%s)", p), input
 }
 
-func attribute(input []byte) (string, []byte) {
-  _, input = token(LBRACKET, input)
-  _, input = token(SPACES, input)
-  name, input := token(ATTR_NAME, input)
+func attribute(matchers []*rubex.Regexp, input []byte) (string, []byte) {
+  _, input = token(matchers, LBRACKET, input)
+  _, input = token(matchers, SPACES, input)
+  name, input := token(matchers, ATTR_NAME, input)
   if name == nil {
     panic("Attribute selector requires an attribute name.")
   }
-  _, input = token(SPACES, input)
-  if rbracket, remainder := token(RBRACKET, input); rbracket != nil {
+  _, input = token(matchers, SPACES, input)
+  if rbracket, remainder := token(matchers, RBRACKET, input); rbracket != nil {
     return "@" + string(name), remainder
   }
-  op, input := token(MATCH_OP, input)
+  op, input := token(matchers, MATCH_OP, input)
   if op == nil {
     panic("Missing operator in attribute selector.")
   }
-  _, input = token(SPACES, input)
-  val, input := token(ATTR_VALUE, input)
+  _, input = token(matchers, SPACES, input)
+  val, input := token(matchers, ATTR_VALUE, input)
   if val == nil {
     panic("Missing value in attribute selector.")
   }
-  _, input = token(SPACES, input)
-  rbracket, input := token(RBRACKET, input)
+  _, input = token(matchers, SPACES, input)
+  rbracket, input := token(matchers, RBRACKET, input)
   if rbracket == nil {
     panic("Unterminated attribute selector.")
   }
@@ -374,8 +385,8 @@ func attribute(input []byte) (string, []byte) {
   return expr, input
 }
 
-func token(lexeme Lexeme, input []byte) ([]byte, []byte) {
-  matched := matcher[lexeme].Find(input)
+func token(matchers []*rubex.Regexp, lexeme Lexeme, input []byte) ([]byte, []byte) {
+  matched := matchers[lexeme].Find(input)
   length := len(matched)
   if length == 0 {
     matched = nil
@@ -383,7 +394,7 @@ func token(lexeme Lexeme, input []byte) ([]byte, []byte) {
   return matched, input[length:]
 }
 
-func peek(lexeme Lexeme, input []byte) bool {
-  matched, _ := token(lexeme, input)
+func peek(matchers []*rubex.Regexp, lexeme Lexeme, input []byte) bool {
+  matched, _ := token(matchers, lexeme, input)
   return matched != nil
 }
