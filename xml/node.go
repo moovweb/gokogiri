1761d056598205491a657f4275025eb868404c70
Joel Reymont <joelr1@gmail.com>
2013-06-19 22:59:57 +0100

    xmlRemoveDefaultNamespace

--- xml/node.go
+++ xml/node.go
@@ -142,6 +142,7 @@ type Node interface {
 
 	RecursivelyRemoveNamespaces() error
 	SetNamespace(string, string)
+	RemoveDefaultNamespace()
 }
 
 //run out of memory
@@ -879,6 +880,11 @@ func (xmlNode *XmlNode) RecursivelyRemoveNamespaces() (err error) {
 		}
 	}
 	return
+}
+
+func (xmlNode *XmlNode) RemoveDefaultNamespace() {
+	nodePtr := xmlNode.Ptr
+	C.xmlRemoveDefaultNamespace(nodePtr)
 }
 
 func (xmlNode *XmlNode) SetNamespace(prefix, href string) {
