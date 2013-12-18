b4cf14401be6633847b30502577eed01cf1976f9
John C Barstow <jbowtie@amathaine.com>
2013-12-18 13:54:43 +1300

    Documentation strings for the gokogiri module and the AttributeNode.

--- xml/attribute.go
+++ xml/attribute.go
@@ -1,17 +1,25 @@
 package xml
 
+/*
+AttributeNode represents an attribute, which has a name and a value.
+*/
 type AttributeNode struct {
 	*XmlNode
 }
 
+// String returns the value of the attribute.
 func (attrNode *AttributeNode) String() string {
 	return attrNode.Content()
 }
 
+// Value returns the value of the attribute.
 func (attrNode *AttributeNode) Value() string {
 	return attrNode.Content()
 }
 
+//SetValue sets the value of the attribute. Note that the argument will
+// be converted to a string, and automatically XML-escaped when the
+// document is serialized.
 func (attrNode *AttributeNode) SetValue(val interface{}) {
 	attrNode.SetContent(val)
 }
